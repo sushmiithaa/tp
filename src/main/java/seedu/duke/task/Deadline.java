f0e72a59e9d8a0327c5bf7a3a2fd5cbd6a9a2917
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-11 17:30:58 +0800

    Deadline class

--- src/main/java/seedu/duke/task/Deadline.java
+++ src/main/java/seedu/duke/task/Deadline.java
@@ -1,4 +1,29 @@
 package seedu.duke.task;
 
-public class Deadline {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Deadline extends Task {
+    protected LocalDateTime by;
+
+    public Deadline(String description, LocalDateTime by) {
+        super(description);
+        this.by = by;
+    }
+
+    public LocalDateTime getBy() {
+        return by;
+    }
+
+    @Override
+    public String toFileFormat() {
+        DateTimeFormatter storageFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return String.format("D | %d | %s | %s", (isDone ? 1 : 0), description, by.format(storageFormatter));
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter displayFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        return "[D]" + super.toString() + " (by: " + by.format(displayFormatter) + ")";
+    }
 }
