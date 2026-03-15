b150e5acde7128d27a07acc5603ba7c4d8fa0670
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-15 23:45:28 +0800

    minor edits to DateUtils, new logging message for incorrect date

--- src/main/java/seedu/duke/util/DateUtils.java
+++ src/main/java/seedu/duke/util/DateUtils.java
@@ -1,12 +1,15 @@
 package seedu.duke.util;
 
 import seedu.duke.exception.IllegalDateException;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.logging.Logger;
 
 public class DateUtils {
+    private static final Logger logger = Logger.getLogger(DateUtils.class.getName());
     private static final DateTimeFormatter FULL_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
     private static final DateTimeFormatter DATE_ONLY_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
@@ -31,6 +34,7 @@ public class DateUtils {
 
         // CRITICAL CHECK
         if (parsedDate.getYear() < 2026) {
+            logger.warning("Rejected date " + trimmedInput + " - Year is before 2026.");
             throw new IllegalDateException("Dates must be in 2026 or later!");
         }
 
