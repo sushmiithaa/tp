54b241fbd6c421a200da7127a5462617bd459bab
Wen Jun Yu <e1406517@u.nus.edu>
2026-03-15 15:21:31 +0800

    JUnit test case for DeadlineList

--- src/test/java/seedu/duke/tasklist/DeadlineListTest.java
+++ src/test/java/seedu/duke/tasklist/DeadlineListTest.java
@@ -31,4 +31,33 @@ public class DeadlineListTest {
         list.delete(0);
         assertEquals(0, list.getSize());
     }
+
+    @Test
+    public void delete_shiftsRemainingTasks_success() {
+        DeadlineList list = new DeadlineList();
+        list.add(new Deadline("First", LocalDateTime.of(2026, 1, 1, 10, 0)));
+        list.add(new Deadline("Second", LocalDateTime.of(2026, 1, 1, 12, 0)));
+
+        list.delete(0); // Remove "First"
+
+        // "Second" should now be at index 0
+        assertEquals(1, list.getSize());
+        assertEquals("Second", list.get(0).getDescription());
+    }
+
+    @Test
+    public void getLatest_afterSort_returnsChronologicallyLatest() {
+        DeadlineList list = new DeadlineList();
+        LocalDateTime early = LocalDateTime.of(2026, 1, 1, 10, 0);
+        LocalDateTime late = LocalDateTime.of(2026, 12, 31, 23, 59);
+
+        // Add late task first, then early task
+        list.add(new Deadline("Late Task", late));
+        list.add(new Deadline("Early Task", early));
+
+        list.sortByDate();
+
+        // After sorting, the late task should be at the end
+        assertEquals("Late Task", list.getLatest().getDescription());
+    }
 }
